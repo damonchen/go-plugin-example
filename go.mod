98f22ab030eacc40f6076a06783c376e71c2a814
damon chen <netubu@gmail.com>
2019-11-23 21:52:21 +0800

    基本可以运行的terraform插件

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.13
 require (
 	github.com/golang/protobuf v1.3.2
 	github.com/hashicorp/go-plugin v1.0.1
+	github.com/hashicorp/terraform-plugin-sdk v1.4.0
 	google.golang.org/grpc v1.25.1
 )
--- terraform-plugin-file/provider.go
+++ terraform-plugin-file/provider.go
@@ -2,20 +2,13 @@ package main
 
 import (
 		"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-		"github.com/hashicorp/terraform-plugin-sdk/plugin"
-        "github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
 func Provider() *schema.Provider {
         return &schema.Provider{
-                ResourcesMap: map[string]*schema.Resource{},
+                ResourcesMap: map[string]*schema.Resource{
+					"example_server": resourceServer(),
+				},
         }
 }
 
-func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: func() terraform.ResourceProvider {
-				return Provider()
-		},
-	})
-}
